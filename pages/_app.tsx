51b3156807a398df075fc9a29713e0522ac420cf
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2023-01-13 10:42:02 +0700

    add google font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Inter, Red_Hat_Display } from '@next/font/google';
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
+const redHat = Red_Hat_Display({ subsets: ['latin'], variable: '--font-red-hat' });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <main className={`${redHat.variable} ${inter.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
